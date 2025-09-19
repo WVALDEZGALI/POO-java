849b2aad15e8f8b597f3c15950696fe8087c3954
William Camilo Valdes Galindo <camiloga17310@gmail.com>
2025-09-19 10:04:32 -0500

    POOV1

---
+++ src/HelloWorldExercises.java
@@ -0,0 +1,27 @@
+public class HelloWorldExercises { //Public class named HelloWorldExercises
+    public static void main(String[] args) { //Main method return void
+        System.out.println("Camilo"); // Print in line my name
+        System.out.println("""
+                Hello
+                World
+                """); // Print in twice lines
+        System.out.println("39");
+        System.out.println("Metallic Blue");
+        System.out.println("Bogota");
+        System.out.println("Beautiful");
+        System.out.println("Day");
+        System.out.println("To");
+        System.out.println("Learn");
+        System.out.println("""
+                   ****
+                 *      *
+                *  0  0  *
+                 *  --   *
+                   ****
+                """);
+
+    } /*Many
+    lines
+    comment*/
+
+}
---
+++ src/VariableAndConstants.java
@@ -0,0 +1,6 @@
+public class VariableAndConstants {
+    public static void main(String[] args) {
+        String name = "Camilo";
+        System.out.println(name);
+    }
+}
