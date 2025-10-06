6ff6cda01319aeb1b7a4687396c395fba79bbedf
William Camilo Valdes Galindo <camiloga17310@gmail.com>
2025-10-06 10:50:32 -0500

    POOV9

---
+++ src/F_Loops/Loops.java
@@ -0,0 +1,18 @@
+package F_Loops;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        // For
+
+        for (int i = 1; i < 6; i++) {
+            System.out.println("Hello JAVA: " + i);
+        }
+
+        String[] names = {"Camilo", "Ana", "Mario", "Olga", "Diego",};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
